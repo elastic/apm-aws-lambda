69b296a08bf903ee76eb4cbe715c18e089333a8e
Alan Storm <alan.storm@elastic.co>
2021-09-27 16:37:14 -0700

    feat: all commands implemented

--- apm-lambda-extension/cli/elastic-lambda.js
+++ apm-lambda-extension/cli/elastic-lambda.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
+const AWS = require('aws-sdk')
 const yargs = require('yargs/yargs')
 const {hideBin} = require('yargs/helpers')
 
-if(!process.env.AWS_DEFAULT_REGION) {
-  console.log('please set AWS_DEFAULT_REGION')
-  process.exit(1)
+function checkAwsRegion() {
+  if(!process.env.AWS_DEFAULT_REGION) {
+    console.log('please set AWS_DEFAULT_REGION')
+    process.exit(1)
+  }
 }
+
 const argv = yargs(hideBin(process.argv)).command(
   'update-layers [function_name] [layer_arn]',
   'updates or adds a layer ARN to a lambda\'s layers',
@@ -15,10 +19,25 @@ const argv = yargs(hideBin(process.argv)).command(
     const {cmd} = require('./update-layers.js')
     cmd(argv)
   }
-  )/*option('region', {
-    alias: 'r',
-    type: 'region',
-    description: 'set aws region'
-  })*/.demandCommand().recommendCommands().strict().parse()
+  ).command(
+    'build-and-publish',
+    'runs build-and-publish make command in ..',
+    function(yargs) {
+    },
+    function(argv) {
+      const {cmd} = require('./build-and-publish')
+      cmd(argv)
+    }
+  ).command(
+    'update-function-env [function_name] [env_as_json]',
+    'adds env configuration to named lambda function',
+    function(yargs) {
+    },
+    function(argv){
+      checkAwsRegion()
+      const {cmd} = require('./update-function-env')
+      cmd(argv)
+    }
+  ).demandCommand().recommendCommands().strict().parse()
 
 
