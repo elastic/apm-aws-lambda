7bfe00a99ff47a2ae5a712ae10f527139f1a4f80
Emily S <emily.stolfo@elastic.co>
2021-10-26 08:26:03 +0200

    Return an error if the logs api is not supported (#64)

--- apm-lambda-extension/main.go
+++ apm-lambda-extension/main.go
@@ -65,22 +65,26 @@ func main() {
 
 	extension.NewHttpServer(dataChannel, config)
 
-	// Subscribe to the Logs API
-	logsapi.Subscribe(
-		extensionClient.ExtensionID,
-		[]logsapi.EventType{logsapi.Platform})
-
 	// Make channel for collecting logs and create a HTTP server to listen for them
 	logsChannel := make(chan logsapi.LogEvent)
-	logsAPIListener, err := logsapi.NewLogsAPIHttpListener(logsChannel)
-	if err != nil {
-		log.Printf("Error while creating Logs API listener: %v", err)
-	}
 
-	// Start the logs HTTP server
-	_, err = logsAPIListener.Start(logsapi.ListenOnAddress())
+	// Subscribe to the Logs API
+	err = logsapi.Subscribe(
+		extensionClient.ExtensionID,
+		[]logsapi.EventType{logsapi.Platform})
 	if err != nil {
-		log.Printf("Error while starting Logs API listener: %v", err)
+		log.Printf("Could not subscribe to the logs API. Will instead flush APM data 100ms before the function deadline.")
+	} else {
+		logsAPIListener, err := logsapi.NewLogsAPIHttpListener(logsChannel)
+		if err != nil {
+			log.Printf("Error while creating Logs API listener: %v", err)
+		}
+
+		// Start the logs HTTP server
+		_, err = logsAPIListener.Start(logsapi.ListenOnAddress())
+		if err != nil {
+			log.Printf("Error while starting Logs API listener: %v", err)
+		}
 	}
 
 	for {
@@ -170,7 +174,7 @@ func main() {
 
 			select {
 			case <-runtimeDone:
-				log.Println("Receive runtimeDone event signal")
+				log.Println("Received runtimeDone event signal")
 			case <-timer.C:
 				log.Println("Time expired waiting for runtimeDone event")
 			}
