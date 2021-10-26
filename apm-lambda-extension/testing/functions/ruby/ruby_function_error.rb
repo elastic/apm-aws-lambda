1ef76fe5f7de64972dfce4c41f289898a130ecdc
Emily Stolfo <emily.stolfo@elastic.co>
2021-10-26 15:03:06 +0200

    Change directory name to testing

--- apm-lambda-extension/testing/functions/ruby/ruby_function_error.rb
+++ apm-lambda-extension/testing/functions/ruby/ruby_function_error.rb
@@ -1,5 +1,6 @@
 load_paths = Dir["vendor/bundle/ruby/2.7.0/bundler/gems/**/lib"]
 load_paths += Dir["vendor/bundle/ruby/2.7.0/gems/**/lib"]
+load_paths += Dir["vendor/bundle/ruby/2.7.0/extensions/x86_64-linux/2.7.0/*"]
 load_paths += Dir["vendor/bundle/ruby/2.7.0/gems/concurrent-ruby-1.1.9/lib/concurrent-ruby"]
 
 $LOAD_PATH.unshift(*load_paths)
@@ -7,8 +8,11 @@ $LOAD_PATH.unshift(*load_paths)
 require 'json'
 require 'elastic-apm'
 
-ElasticAPM.start(pool_size: 2)
+ElasticAPM.start(pool_size: 2, service_name: 'LocalLambdaTesting')
 
 def lambda_handler(event:, context:)
   raise Exception
+rescue => e
+  ElasticAPM.flush
+  raise e
 end
