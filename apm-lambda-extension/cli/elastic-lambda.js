2808a6d5cb5c9d3eee98308087d33439cdd45390
Alan Storm <alan.storm@elastic.co>
2021-11-22 12:08:32 -0800

    feat: initial commit

--- apm-lambda-extension/cli/elastic-lambda.js
+++ apm-lambda-extension/cli/elastic-lambda.js
@@ -64,6 +64,15 @@ const argv = yargs(hideBin(process.argv)).command(
       const {cmd} = require('./install')
       cmd(argv)
     }
+  ).command(
+    'profile',
+    'runs the profiler based on configuration in profile.yaml',
+    function(yargs) {
+    },
+    function(argv) {
+      const {cmd} = require('./profile')
+      cmd(argv)
+    }
   ).demandCommand().recommendCommands().strict().parse()
 
 
---
+++ apm-lambda-extension/cli/profile.js
@@ -0,0 +1,8 @@
+'use strict'
+function cmd() {
+
+}
+
+module.exports = {
+  cmd
+}
