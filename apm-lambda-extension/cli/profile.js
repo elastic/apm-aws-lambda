3d17504cb3e33fc9c153fc310df30ed35b463931
Alan Storm <alan.storm@elastic.co>
2021-11-29 22:53:25 -0800

    fix: code from wrong branch

--- apm-lambda-extension/cli/profile.js
+++ apm-lambda-extension/cli/profile.js
@@ -3,6 +3,7 @@ const yaml = require('js-yaml')
 const AWS = require('aws-sdk')
 const fs = require('fs')
 const { exec, /*execFile*/ } = require("child_process")
+const { exit } = require('process')
 
 AWS.config.update({region: 'us-west-2'});
 const lambda = new AWS.Lambda({apiVersion:'2015-03-31'})
@@ -36,32 +37,38 @@ function createFunction(args) {
   return lambda.createFunction(args).promise()
 }
 
-async function cleanup() {
+async function cleanup(functionName) {
   const deleteFunction = await lambda.deleteFunction({
-    FunctionName: 'otel-automatic-test'
+    FunctionName: functionName
   }).promise();
 }
 
+function generateTmpFunctionName(prefix) {
+  const maxLengthLambda = 64
+  const name = [prefix,'apm-profile'].join('')
+  if(name.length > maxLengthLambda) {
+    console.log(`final function name ${name} is too long, bailing.`);
+    process.exit(1)
+  }
+  return name
+}
+
 async function cmd() {
+  const config = yaml.load(fs.readFileSync(__dirname + '/profile.yaml')).profile
+  const scenario = config.scenarios.otel
+  const functionName = generateTmpFunctionName(scenario.function_name_prefix)
+
   let createFunctionPromise = createFunction({
-    FunctionName: 'otel-automatic-test',
-    Role: 'arn:aws:iam::627286350134:role/service-role/http-api-node-role-egndxmyl',
+    FunctionName: functionName,
+    Role: scenario.role,
     Code: {
-      ZipFile: fs.readFileSync(__dirname + '/code.zip'),
+      ZipFile: fs.readFileSync(__dirname + '/' + scenario.code),
     },
-    Handler: 'index.handler',
-    Runtime: 'nodejs14.x',
-    Layers: [
-      'arn:aws:lambda:us-west-2:627286350134:layer:otel-collector:1',
-      'arn:aws:lambda:us-west-2:627286350134:layer:otel-nodejs:1'
-    ],
+    Handler: scenario.handler,
+    Runtime: scenario.runtime,
+    Layers: scenario.layer_arns,
     "Environment": {
-      "Variables": {
-        'AWS_LAMBDA_EXEC_WRAPPER':'/opt/otel-handler',
-        'OPENTELEMETRY_COLLECTOR_CONFIG_FILE':'/var/task/collector.yaml',
-        'OTEL_EXPORTER_OTLP_ENDPOINT':'http://localhost:55681/v1/traces',
-        'OTEL_TRACES_SAMPLER':'AlwaysOn',
-      }
+      "Variables": scenario.environment.variables
     },
   })
 
@@ -76,7 +83,7 @@ async function cmd() {
     async function waitUntilNotPending(toRun, times=0) {
       const maxTimes = 10
       const configuration = await lambda.getFunctionConfiguration({
-        FunctionName: 'otel-automatic-test'
+        FunctionName: functionName
       }).promise()
 
       if(configuration.State === "Pending" && times <= maxTimes) {
@@ -96,17 +103,20 @@ async function cmd() {
     }
     waitUntilNotPending(function() {
       // invoke test runner here
-      process.env['AWS_PROFILE'] = 'default'
-      exec('/usr/local/opt/openjdk/bin/java -jar ' +
-           __dirname + '/lpt-0.1.jar -n 10 ' +
-           `-a ${resultCreateFunction.FunctionArn} ` +
+      const times = config.config.n
+
+      console.log(`Running profile command with -n ${times} (this may take a while)`);
+
+      const env = Object.assign({}, process.env)
+      exec(`${config.config.path_java} -jar ` +
+           `${config.config.path_lpt_jar} -n ${times} ` +
            `-a ${resultCreateFunction.FunctionArn} `,
-           {env:{AWS_PROFILE:'default', AWS_DEFAULT_REGION: 'us-west-2'}},
+           env,
            function(error, stdout, stderr) {
              console.log("command done")
              console.log(stdout)
              console.log(convertStdoutTableToObject(stdout))
-             cleanup()
+             cleanup(functionName)
            }
       )
     })
@@ -114,7 +124,9 @@ async function cmd() {
     console.log('Error creating function')
     if(e.statusCode === 409 && e.code === 'ResourceConflictException') {
       console.log('Function already exists, deleting. Rerun profiler.')
-      cleanup()
+      cleanup(functionName)
+    } else {
+      console.log(e)
     }
   })
 
@@ -127,5 +139,8 @@ async function cmd() {
 }
 
 module.exports = {
-  cmd
+  cmd,
+
+  // exported for testing
+  convertStdoutTableToObject
 }
