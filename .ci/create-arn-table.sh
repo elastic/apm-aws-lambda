97e47db02ba85c2a7b6c6ab88bc50758cea518dc
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-01-13 17:10:54 +0000

    ci: create ARN table for each arch

---
+++ .ci/create-arn-table.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+#
+# Create the AWS ARN table given the below environment variables:
+#
+#   - AWS_FOLDER      - that's the location of the publish-layer-version output for each region
+#	- GOARCH          - that's the supported architecture.
+#	- PREFIX_ARN_FILE - that's the output file.
+#
+
+{
+	echo "### ARCH: ${GOARCH}"
+	echo ''
+	echo '|Region|Arch|ARN|'
+	echo '|------|----|---|'
+	for f in $(ls "${AWS_FOLDER}"); do
+		# TODO: identify what field to be used.
+		echo "|${f}|${GOARCH}|$(cat $AWS_FOLDER/${f} | jq -r .LayerName)|"
+	done
+	echo ''
+} > ${GOARCH}-${PREFIX_ARN_FILE}
