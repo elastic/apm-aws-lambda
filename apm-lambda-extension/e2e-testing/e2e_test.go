cdffa11ecf16eb3d9aa2af16b7baac66546d3871
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2022-01-26 19:51:09 +0100

    Replace Elasticsearch by a mock APM server

--- apm-lambda-extension/e2e-testing/e2e_test.go
+++ apm-lambda-extension/e2e-testing/e2e_test.go
@@ -3,9 +3,10 @@ package e2e_testing
 import (
 	"archive/zip"
 	"bufio"
-	"encoding/json"
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"fmt"
-	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
@@ -13,6 +14,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -29,27 +31,12 @@ func TestEndToEndExtensionBehavior(t *testing.T) {
 	if os.Getenv("RUN_E2E_TESTS") != "true" {
 		t.Skip("Skipping E2E tests. Please set the env. variable RUN_E2E_TESTS=true if you want to run them.")
 	}
-	if os.Getenv("ELASTIC_CLOUD_ID") == "" {
-		log.Println("Please set the ELASTIC_CLOUD_ID environment variable")
-		os.Exit(1)
-	}
-	if os.Getenv("ELASTIC_CLOUD_API_KEY") == "" {
-		log.Println("Please set the ELASTIC_CLOUD_API_KEY environment variable")
-		os.Exit(1)
-	}
-	if os.Getenv("ELASTIC_APM_SERVER_URL") == "" {
-		log.Println("Please set the ELASTIC_APM_SERVER_URL environment variable")
-		os.Exit(1)
-	}
-	if os.Getenv("ELASTIC_APM_SERVER_TOKEN") == "" {
-		log.Println("Please set the ELASTIC_APM_SERVER_TOKEN environment variable")
-		os.Exit(1)
-	}
 	forceBuildLambdasFlag := false
 	if os.Getenv("FORCE_REBUILD_LOCAL_SAM_LAMBDAS") == "true" {
 		forceBuildLambdasFlag = true
 	}
 
+	// Build and download required binaries (extension and Java agent)
 	buildExtensionBinaries()
 	if !folderExists(filepath.Join("sam-java", "agent")) {
 		log.Println("Java agent not found ! Collecting archive from Github...")
@@ -57,34 +44,53 @@ func TestEndToEndExtensionBehavior(t *testing.T) {
 	}
 	changeJavaAgentPermissions("sam-java")
 
+	mockAPMServerLog := ""
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.RequestURI == "/intake/v2/events" {
+			mockAPMServerLog += decodeRequest(r)
+		}
+	}))
+	defer ts.Close()
+
 	if os.Getenv("PARALLEL_EXECUTION") == "true" {
 		var waitGroup sync.WaitGroup
 		waitGroup.Add(3)
 
-		cNode := make(chan bool)
-		go runTestAsync("sam-node", "SamTestingNode", forceBuildLambdasFlag, &waitGroup, cNode)
-		cPython := make(chan bool)
-		go runTestAsync("sam-python", "SamTestingPython", forceBuildLambdasFlag, &waitGroup, cPython)
-		cJava := make(chan bool)
-		go runTestAsync("sam-java", "SamTestingJava", forceBuildLambdasFlag, &waitGroup, cJava)
-		assert.True(t, <-cNode)
-		assert.True(t, <-cPython)
-		assert.True(t, <-cJava)
+		cNode := make(chan string)
+		go runTestAsync("sam-node", "SamTestingNode", ts.URL, forceBuildLambdasFlag, &waitGroup, cNode)
+		cPython := make(chan string)
+		go runTestAsync("sam-python", "SamTestingPython", ts.URL, forceBuildLambdasFlag, &waitGroup, cPython)
+		cJava := make(chan string)
+		go runTestAsync("sam-java", "SamTestingJava", ts.URL, forceBuildLambdasFlag, &waitGroup, cJava)
+		uuidNode := <-cNode
+		uuidPython := <-cPython
+		uuidJava := <-cJava
+		log.Printf("Querying the mock server for transaction %s bound to %s...", uuidNode, "SamTestingNode")
+		assert.True(t, strings.Contains(mockAPMServerLog, uuidNode))
+		log.Printf("Querying the mock server for transaction %s bound to %s...", uuidPython, "SamTestingPython")
+		assert.True(t, strings.Contains(mockAPMServerLog, uuidPython))
+		log.Printf("Querying the mock server for transaction %s bound to %s...", uuidJava, "SamTestingJava")
+		assert.True(t, strings.Contains(mockAPMServerLog, uuidJava))
 		waitGroup.Wait()
 
 	} else {
-		assert.True(t, runTest("sam-node", "SamTestingNode", forceBuildLambdasFlag))
-		assert.True(t, runTest("sam-python", "SamTestingPython", forceBuildLambdasFlag))
-		assert.True(t, runTest("sam-java", "SamTestingJava", forceBuildLambdasFlag))
+		uuidNode := runTest("sam-node", "SamTestingNode", ts.URL, forceBuildLambdasFlag)
+		uuidPython := runTest("sam-python", "SamTestingPython", ts.URL, forceBuildLambdasFlag)
+		uuidJava := runTest("sam-java", "SamTestingJava", ts.URL, forceBuildLambdasFlag)
+		log.Printf("Querying the mock server logs for transaction %s bound to %s...", uuidNode, "SamTestingNode")
+		assert.True(t, strings.Contains(mockAPMServerLog, uuidNode))
+		log.Printf("Querying the mock server logs for transaction %s bound to %s...", uuidPython, "SamTestingPython")
+		assert.True(t, strings.Contains(mockAPMServerLog, uuidPython))
+		log.Printf("Querying the mock server logs for transaction %s bound to %s...", uuidJava, "SamTestingJava")
+		assert.True(t, strings.Contains(mockAPMServerLog, uuidJava))
 	}
-
 }
 
 func buildExtensionBinaries() {
 	runCommandInDir("make", []string{}, "..", os.Getenv("DEBUG_OUTPUT") == "true")
 }
 
-func runTest(path string, serviceName string, buildFlag bool) bool {
+func runTest(path string, serviceName string, serverURL string, buildFlag bool) string {
 	log.Printf("Starting to test %s", serviceName)
 
 	if !folderExists(filepath.Join(path, ".aws-sam")) || buildFlag {
@@ -94,26 +100,20 @@ func runTest(path string, serviceName string, buildFlag bool) bool {
 
 	log.Printf("Invoking the Lambda function %s", serviceName)
 	uuidWithHyphen := uuid.New().String()
+	urlSlice := strings.Split(serverURL, ":")
+	port := urlSlice[len(urlSlice)-1]
 	runCommandInDir("sam", []string{"local", "invoke", "--parameter-overrides",
-		fmt.Sprintf("ParameterKey=ApmServerURL,ParameterValue=%s", os.Getenv("ELASTIC_APM_SERVER_URL")),
-		fmt.Sprintf("ParameterKey=ApmSecretToken,ParameterValue=%s", os.Getenv("ELASTIC_APM_SERVER_TOKEN")),
+		fmt.Sprintf("ParameterKey=ApmServerURL,ParameterValue=http://host.docker.internal:%s", port),
 		fmt.Sprintf("ParameterKey=TestUUID,ParameterValue=%s", uuidWithHyphen)},
 		path, os.Getenv("DEBUG_OUTPUT") == "true")
 	log.Printf("%s execution complete", serviceName)
 
-	log.Printf("Querying Elasticsearch for transaction %s bound to %s...", uuidWithHyphen, serviceName)
-	res := queryElasticsearch(serviceName, uuidWithHyphen)
-	if res {
-		log.Printf("SUCCESS : Transaction %s successfully sent to Elasticsearch by %s", uuidWithHyphen, serviceName)
-		return true
-	}
-	log.Printf("FAILURE : Transaction %s bound to %s not found in Elasticsearch", uuidWithHyphen, serviceName)
-	return false
+	return uuidWithHyphen
 }
 
-func runTestAsync(path string, serviceName string, buildFlag bool, wg *sync.WaitGroup, c chan bool) {
+func runTestAsync(path string, serviceName string, serverURL string, buildFlag bool, wg *sync.WaitGroup, c chan string) {
 	defer wg.Done()
-	out := runTest(path, serviceName, buildFlag)
+	out := runTest(path, serviceName, serverURL, buildFlag)
 	c <- out
 }
 
@@ -146,52 +146,6 @@ func changeJavaAgentPermissions(samJavaPath string) {
 	for _, f := range agentFiles {
 		os.Chmod(filepath.Join(agentFolderPath, f.Name()), 0755)
 	}
-}
-
-func queryElasticsearch(serviceName string, transactionName string) bool {
-
-	cfg := elasticsearch.Config{
-		CloudID: os.Getenv("ELASTIC_CLOUD_ID"),
-		APIKey:  os.Getenv("ELASTIC_CLOUD_API_KEY"),
-	}
-
-	es, err := elasticsearch.NewClient(cfg)
-
-	body := fmt.Sprintf(`{
-  				"query": {
-    				"bool": {
-      					"must": [
-        					{
-          						"match": {
-            						"service.name": "%s"
-          						}
-        					},
-        					{
-          						"match": {
-            						"transaction.name": "%s"
-          						}
-        					}
-      					]
-    				}
-  				}
-			}`, serviceName, transactionName)
-
-	res, err := es.Count(
-		es.Count.WithIndex("apm-*-transaction-*"),
-		es.Count.WithBody(strings.NewReader(body)),
-		es.Count.WithPretty(),
-	)
-	processError(err)
-	defer res.Body.Close()
-
-	var r map[string]interface{}
-	err = json.NewDecoder(res.Body).Decode(&r)
-	processError(err)
-	hitsNum := int(r["count"].(float64))
-	if hitsNum > 0 {
-		return true
-	}
-	return false
 }
 
 func runCommandInDir(command string, args []string, dir string, printOutput bool) {
@@ -283,4 +237,15 @@ func unzip(archivePath string, destinationFolderPath string) {
 		err := extractAndWriteFile(f)
 		processError(err)
 	}
+}
+
+func decodeRequest(r *http.Request) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	str := base64.StdEncoding.EncodeToString(buf.Bytes())
+	data, _ := base64.StdEncoding.DecodeString(str)
+	rdata := bytes.NewReader(data)
+	reader, _ := gzip.NewReader(rdata)
+	s, _ := ioutil.ReadAll(reader)
+	return string(s)
 }
