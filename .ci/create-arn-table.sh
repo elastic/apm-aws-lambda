a92b6ee7257b091c8eedfe7195a7203771c410d3
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-04 18:53:01 +0000

    use grep/sed since jq failed in the CI

--- .ci/create-arn-table.sh
+++ .ci/create-arn-table.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -o pipefail
 
 #
 # Create the AWS ARN table given the below environment variables:
@@ -19,7 +19,7 @@ ARN_FILE=${ARCHITECTURE}-${SUFFIX_ARN_FILE}
 } > "${ARN_FILE}"
 
 for f in $(ls "${AWS_FOLDER}"); do
-	LAYER_VERSION_ARN=$(jq -r .LayerVersionArn "$AWS_FOLDER/${f}")
+	LAYER_VERSION_ARN=$(grep '"LayerVersionArn"' "$AWS_FOLDER/${f}" | cut -d":" -f2- | sed 's/ //g' | sed 's/"//g' | cut -d"," -f1)
 	echo "INFO: create-arn-table ARN(${LAYER_VERSION_ARN}):region(${f}):arch(${ARCHITECTURE})"
 	echo "|${f}|${ARCHITECTURE}|${LAYER_VERSION_ARN}|" >> "${ARN_FILE}"
 done
