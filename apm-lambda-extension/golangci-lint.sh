8907a3a836d65e2a092245870a4166e9c431314c
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2022-03-24 11:48:43 +0100

    Simplify shell script

--- apm-lambda-extension/golangci-lint.sh
+++ apm-lambda-extension/golangci-lint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.45.0
 golangci-lint --version
-golangci-lint run | read && echo "Code differs from golangci-lint style. Run 'golangci-lint run'" 1>&2 && exit 1 || true
+golangci-lint run
