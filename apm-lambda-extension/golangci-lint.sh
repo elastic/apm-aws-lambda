36b41f1fc9261215cfa1ddfccb56ea5209e3127e
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2022-03-24 17:33:40 +0100

    Delete golangci-lint.sh

--- apm-lambda-extension/golangci-lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.45.0
-golangci-lint --version
-golangci-lint run
