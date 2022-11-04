fc04af036a2e38155064bd9f980cf0a846c39944
Vishal Raj <vishal.raj@elastic.co>
2022-11-04 15:46:49 +0800

    Update makefile and fix go fn req ID handling

--- testing/functions/go1.x/main.go
+++ testing/functions/go1.x/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 
 	_ "go.elastic.co/apm/module/apmlambda/v2"
 )
 
 var coldstart = true
 
-func Handle(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Println("Example function log", req.RequestContext.RequestID)
+func Handle(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	lc, _ := lambdacontext.FromContext(ctx)
+	log.Println("Example function log", lc.AwsRequestID)
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("Hello world %s!", req.RequestContext.RequestID),
+		Body:       fmt.Sprintf("Hello from go!%s", lc.AwsRequestID),
 		Headers: map[string]string{
 			"coldstart": strconv.FormatBool(coldstart),
 		},
