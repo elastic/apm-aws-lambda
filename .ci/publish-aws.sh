c068bdcc55caa2f56c75124561a1dbf97d408837
Jan Calanog <jan.calanog@elastic.co>
2023-02-22 11:13:49 +0100

    Add comment

--- .ci/publish-aws.sh
+++ .ci/publish-aws.sh
@@ -8,6 +8,10 @@ set -euo pipefail
 
 export AWS_FOLDER=${AWS_FOLDER:-.aws}
 export SUFFIX_ARN_FILE=${SUFFIX_ARN_FILE:-arn-file.md}
+
+# This needs to be set in GH actions
+# https://dotjoeblog.wordpress.com/2021/03/14/github-actions-aws-error-exit-code-255/
+# eu-west-1 is just a random region
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-eu-west-1}
 
 GOOS=${GOOS:?Please provide GOOS environment variable.}
