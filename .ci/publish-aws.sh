1d46f1ee04b1114939089bbea97f13fd895e104a
Jan Calanog <jan.calanog@elastic.co>
2023-02-22 11:26:02 +0100

    Fix release workflow (#380)
    * Fix aws cli usage
    
    Related to https://dotjoeblog.wordpress.com/2021/03/14/github-actions-aws-error-exit-code-255/
    
    * Add comment

--- .ci/publish-aws.sh
+++ .ci/publish-aws.sh
@@ -9,6 +9,11 @@ set -euo pipefail
 export AWS_FOLDER=${AWS_FOLDER:-.aws}
 export SUFFIX_ARN_FILE=${SUFFIX_ARN_FILE:-arn-file.md}
 
+# This needs to be set in GH actions
+# https://dotjoeblog.wordpress.com/2021/03/14/github-actions-aws-error-exit-code-255/
+# eu-west-1 is just a random region
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-eu-west-1}
+
 GOOS=${GOOS:?Please provide GOOS environment variable.}
 GOARCH=${GOARCH:?Please provide GOARCH environment variable.}
 ELASTIC_LAYER_NAME=${ELASTIC_LAYER_NAME:?Please provide ELASTIC_LAYER_NAME environment variable.}
