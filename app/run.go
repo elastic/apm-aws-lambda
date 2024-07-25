22d6d3e0a7189817f6f041466abfe199c193834e
kruskal <99559985+kruskall@users.noreply.github.com>
2024-07-25 10:56:31 +0200

    fix: only flush logs if logs collection is enabled

--- app/run.go
+++ app/run.go
@@ -96,8 +96,10 @@ func (app *App) Run(ctx context.Context) error {
 			backgroundDataSendWg.Wait()
 			if event.EventType == extension.Shutdown {
 				app.logger.Infof("Exiting due to shutdown event with reason %s", event.ShutdownReason)
-				// Flush buffered logs if any
-				app.logsClient.FlushData(ctx, event.RequestID, event.InvokedFunctionArn, app.apmClient.LambdaDataChannel, true)
+				if app.logsClient != nil {
+					// Flush buffered logs if any
+					app.logsClient.FlushData(ctx, event.RequestID, event.InvokedFunctionArn, app.apmClient.LambdaDataChannel, true)
+				}
 				// Since we have waited for the processEvent loop to finish we
 				// already have received all the data we can from the agent. So, we
 				// flush all the data to make sure that shutdown can correctly deduce
@@ -127,8 +129,10 @@ func (app *App) Run(ctx context.Context) error {
 				// that the underlying transport is reset for next invocation without
 				// waiting for grace period if it got to unhealthy state.
 				flushCtx, cancel := context.WithCancel(ctx)
-				// Flush buffered logs if any
-				app.logsClient.FlushData(ctx, event.RequestID, event.InvokedFunctionArn, app.apmClient.LambdaDataChannel, false)
+				if app.logsClient != nil {
+					// Flush buffered logs if any
+					app.logsClient.FlushData(ctx, event.RequestID, event.InvokedFunctionArn, app.apmClient.LambdaDataChannel, false)
+				}
 				// Flush APM data now that the function invocation has completed
 				app.apmClient.FlushAPMData(flushCtx)
 				cancel()
