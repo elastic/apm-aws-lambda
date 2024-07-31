40f6c6bb39363d2a0eab8c7dcd70212c98832828
Tim Rühsen <tim.ruhsen@elastic.co>
2024-07-31 16:27:39 +0200

    test: Use hard-coded user_name in CI
    The gh tool doesn't has permissions in CI and a using a hard-coded
    user_name is sufficient.

--- testing/smoketest/get_username.sh
+++ testing/smoketest/get_username.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eu
 
+: ${CI:=""}
+
+if [ -z "$CI" ]; then
+  user_name="github-actions"
 # The gh command is faster and not as hacky as the fallback solution
-if which gh >/dev/null; then
+elif which gh >/dev/null; then
   user_name=$(gh api user -q ".login")
 else
   user_name=$(ssh -T git@github.com 2>&1|cut -d'!' -f1|cut -d' ' -f2)
