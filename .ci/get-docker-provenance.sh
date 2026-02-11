8ff22b5f75c5fd05207b099cd42efc15e5b6814f
Victor Martinez <VictorMartinezRubio@gmail.com>
2026-02-11 13:28:43 +0100

    chore

--- .ci/get-docker-provenance.sh
+++ .ci/get-docker-provenance.sh
@@ -18,5 +18,5 @@ digest_2=$(echo "$images" | jq -r '.[1].extra.Digest')
 echo "Export github actions outputs"
 echo "name_1=$image_1"    >> "$GITHUB_OUTPUT"
 echo "name_2=$image_2"    >> "$GITHUB_OUTPUT"
-echo "digest_2=$digest_2" >> "$GITHUB_OUTPUT"
 echo "digest_1=$digest_1" >> "$GITHUB_OUTPUT"
+echo "digest_2=$digest_2" >> "$GITHUB_OUTPUT"
